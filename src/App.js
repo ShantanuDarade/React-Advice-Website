60dccb379094733b850ba24facc4d1c47a2ea85f
shantanu darade <shantanudarade717@gmail.com>
2023-10-05 16:29:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const API_URL = 'https://api.adviceslip.com/advice'
+
+  const [advice, setAdvice] = useState()
+  
+  async function getData(url) {
+    try {
+      const response = await axios.get(url)
+      const advice = response.data.slip.advice
+      setAdvice(advice)
+    } 
+    catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+      getData(API_URL)
+  }, [])
+
+  function changeAdvice() {
+    getData(API_URL)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='center'>
+        <h2>{advice}</h2>
+        <button onClick={changeAdvice}>Change</button>
     </div>
   );
 }
